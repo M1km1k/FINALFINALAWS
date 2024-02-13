4dfc9d0c1fda67119ae87b051160e286acf29b69
Great Chad <125615782+M1km1k@users.noreply.github.com>
2024-02-13 16:28:49 +0800

    Add files via upload

---
+++ Fixed Exercises Final Final/BasicPersonalInfo/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace BasicPersonalInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.txtMiddlename = new System.Windows.Forms.TextBox();
+            this.txtFirstname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.civilStatus = new System.Windows.Forms.ComboBox();
+            this.cbBadminton = new System.Windows.Forms.CheckBox();
+            this.cbVolleyball = new System.Windows.Forms.CheckBox();
+            this.cbBasketball = new System.Windows.Forms.CheckBox();
+            this.rbtMale = new System.Windows.Forms.RadioButton();
+            this.rbtFemale = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtBdate = new System.Windows.Forms.DateTimePicker();
+            this.Gwapo = new System.Windows.Forms.PictureBox();
+            this.BtnDisplayClick = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Gwapo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtLastname);
+            this.groupBox1.Controls.Add(this.txtMiddlename);
+            this.groupBox1.Controls.Add(this.txtFirstname);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(274, 182);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Personal Information";
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastname.Location = new System.Drawing.Point(108, 131);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(154, 18);
+            this.txtLastname.TabIndex = 1;
+            // 
+            // txtMiddlename
+            // 
+            this.txtMiddlename.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMiddlename.Location = new System.Drawing.Point(108, 84);
+            this.txtMiddlename.Name = "txtMiddlename";
+            this.txtMiddlename.Size = new System.Drawing.Size(154, 18);
+            this.txtMiddlename.TabIndex = 1;
+            // 
+            // txtFirstname
+            // 
+            this.txtFirstname.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstname.Location = new System.Drawing.Point(108, 41);
+            this.txtFirstname.Name = "txtFirstname";
+            this.txtFirstname.Size = new System.Drawing.Size(154, 18);
+            this.txtFirstname.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Lastname";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Middlename";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Firstname";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.civilStatus);
+            this.groupBox2.Controls.Add(this.cbBadminton);
+            this.groupBox2.Controls.Add(this.cbVolleyball);
+            this.groupBox2.Controls.Add(this.cbBasketball);
+            this.groupBox2.Controls.Add(this.rbtMale);
+            this.groupBox2.Controls.Add(this.rbtFemale);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.dtBdate);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 236);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(478, 245);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Additional Information";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // civilStatus
+            // 
+            this.civilStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.civilStatus.FormattingEnabled = true;
+            this.civilStatus.Items.AddRange(new object[] {
+            "Single",
+            "Married",
+            "Divorced",
+            "Prefer not to say"});
+            this.civilStatus.Location = new System.Drawing.Point(88, 87);
+            this.civilStatus.Name = "civilStatus";
+            this.civilStatus.Size = new System.Drawing.Size(157, 21);
+            this.civilStatus.TabIndex = 4;
+            // 
+            // cbBadminton
+            // 
+            this.cbBadminton.AutoSize = true;
+            this.cbBadminton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbBadminton.Location = new System.Drawing.Point(90, 206);
+            this.cbBadminton.Name = "cbBadminton";
+            this.cbBadminton.Size = new System.Drawing.Size(86, 19);
+            this.cbBadminton.TabIndex = 3;
+            this.cbBadminton.Text = "Badminton";
+            this.cbBadminton.UseVisualStyleBackColor = true;
+            // 
+            // cbVolleyball
+            // 
+            this.cbVolleyball.AutoSize = true;
+            this.cbVolleyball.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbVolleyball.Location = new System.Drawing.Point(213, 176);
+            this.cbVolleyball.Name = "cbVolleyball";
+            this.cbVolleyball.Size = new System.Drawing.Size(78, 19);
+            this.cbVolleyball.TabIndex = 3;
+            this.cbVolleyball.Text = "Volleyball";
+            this.cbVolleyball.UseVisualStyleBackColor = true;
+            // 
+            // cbBasketball
+            // 
+            this.cbBasketball.AutoSize = true;
+            this.cbBasketball.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbBasketball.Location = new System.Drawing.Point(90, 176);
+            this.cbBasketball.Name = "cbBasketball";
+            this.cbBasketball.Size = new System.Drawing.Size(83, 19);
+            this.cbBasketball.TabIndex = 3;
+            this.cbBasketball.Text = "Basketball";
+            this.cbBasketball.UseVisualStyleBackColor = true;
+            // 
+            // rbtMale
+            // 
+            this.rbtMale.AutoSize = true;
+            this.rbtMale.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtMale.Location = new System.Drawing.Point(90, 132);
+            this.rbtMale.Name = "rbtMale";
+            this.rbtMale.Size = new System.Drawing.Size(53, 19);
+            this.rbtMale.TabIndex = 2;
+            this.rbtMale.TabStop = true;
+            this.rbtMale.Text = "Male";
+            this.rbtMale.UseVisualStyleBackColor = true;
+            // 
+            // rbtFemale
+            // 
+            this.rbtFemale.AutoSize = true;
+            this.rbtFemale.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtFemale.Location = new System.Drawing.Point(213, 131);
+            this.rbtFemale.Name = "rbtFemale";
+            this.rbtFemale.Size = new System.Drawing.Size(67, 19);
+            this.rbtFemale.TabIndex = 2;
+            this.rbtFemale.TabStop = true;
+            this.rbtFemale.Text = "Female";
+            this.rbtFemale.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(14, 180);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 16);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Sports";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(14, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 16);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(14, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Civil Status";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Birthdate";
+            // 
+            // dtBdate
+            // 
+            this.dtBdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtBdate.Location = new System.Drawing.Point(89, 43);
+            this.dtBdate.Name = "dtBdate";
+            this.dtBdate.Size = new System.Drawing.Size(202, 20);
+            this.dtBdate.TabIndex = 0;
+            // 
+            // Gwapo
+            // 
+            this.Gwapo.Image = ((System.Drawing.Image)(resources.GetObject("Gwapo.Image")));
+            this.Gwapo.Location = new System.Drawing.Point(312, 28);
+            this.Gwapo.Name = "Gwapo";
+            this.Gwapo.Size = new System.Drawing.Size(159, 153);
+            this.Gwapo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Gwapo.TabIndex = 2;
+            this.Gwapo.TabStop = false;
+            // 
+            // BtnDisplayClick
+            // 
+            this.BtnDisplayClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDisplayClick.Location = new System.Drawing.Point(350, 187);
+            this.BtnDisplayClick.Name = "BtnDisplayClick";
+            this.BtnDisplayClick.Size = new System.Drawing.Size(82, 23);
+            this.BtnDisplayClick.TabIndex = 3;
+            this.BtnDisplayClick.Text = "Display";
+            this.BtnDisplayClick.UseVisualStyleBackColor = true;
+            this.BtnDisplayClick.Click += new System.EventHandler(this.BtnDisplayClick_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 502);
+            this.Controls.Add(this.BtnDisplayClick);
+            this.Controls.Add(this.Gwapo);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Personal Information";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Gwapo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.TextBox txtMiddlename;
+        private System.Windows.Forms.TextBox txtFirstname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtBdate;
+        private System.Windows.Forms.CheckBox cbBadminton;
+        private System.Windows.Forms.CheckBox cbVolleyball;
+        private System.Windows.Forms.CheckBox cbBasketball;
+        private System.Windows.Forms.RadioButton rbtMale;
+        private System.Windows.Forms.RadioButton rbtFemale;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox civilStatus;
+        private System.Windows.Forms.PictureBox Gwapo;
+        private System.Windows.Forms.Button BtnDisplayClick;
+    }
+}
+
---
+++ Fixed Exercises Final Final/BasicPersonalInfo/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicPersonalInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnDisplayClick_Click(object sender, EventArgs e)
+        {
+
+            string det = "Firstname: " + txtFirstname.Text +
+                "\nMiddlename: " + txtMiddlename.Text +
+                "\nLastname: " + txtLastname.Text +
+                "\nBirthdate: " + dtBdate.Text +
+                "\nCivil Status: " + civilStatus.SelectedItem;
+
+
+            if (rbtMale.Checked)
+                det = det + "\nGender: Male";
+            else
+                det = det + "\nGender: Female";
+
+            det = det + "\n\nSports: ";
+
+            if (cbBasketball.Checked)
+                det = det + "\nBasketball";
+            if (cbBadminton.Checked)
+                det = det + "\nBadminton";
+            if (cbVolleyball.Checked)
+                det = det + "\nVolleyball";
+
+
+
+            MessageBox.Show(det, "Personal Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
